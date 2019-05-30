2faf825347bdb9c8465c817824f1f8a35ee2cae0
Will Washkuhn <will.washkuhn@gmail.com>
2019-05-30 07:46:48 -0500

    Added search box

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import Sports from './components/sports';
 
 class App extends Component {
   state = {
-    query: "BALL",
+    query: "",
     filteredData: [],
     sports: []
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    this.getData();
+  }
+
+  handleInputChange = event => {
+    const query = event.target.value;
+
+    this.setState(prevState => {
+      const filteredData = prevState.sports.filter(sport => {
+        return sport.strSport.toLowerCase().includes(query.toLowerCase());
+      });
+
+      return {
+        query,
+        filteredData
+      };
+    });
+  };
+
+  getData = () => {
     fetch('https://www.thesportsdb.com/api/v1/json/1/all_sports.php')
     .then(res => res.json())
     .then((data) => {
@@ -22,11 +41,20 @@ class App extends Component {
        });
     })
     .catch(console.log)
-  }
+  };
 
   render () {
     return (
-      <Sports sports={this.state.filteredData} />
+      <div className="searchForm">
+        <form>
+          <input
+            placeholder="Search for..."
+            value={this.state.query}
+            onChange={this.handleInputChange}
+          />
+        </form>
+        <Sports sports={this.state.filteredData} />
+      </div>
     );
   }
 }
